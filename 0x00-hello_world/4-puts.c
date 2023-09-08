38abcf0058e333832f9fb63c26ae1fdf09b2dec2
AbdelkaderKHEZOUR <khabdelkaderdjilani@gmail.com>
2023-09-08 15:59:08 -0700

    all tasks done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * * my first c programming file
+ * */
+
+int main(void)
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * * another programm fot printing
+ * * Return: return value is 0
+ * */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
