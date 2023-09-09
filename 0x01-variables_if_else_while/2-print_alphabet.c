9bb2f2d44084415af398ea1b3a54daa3227eea0b
AbdelkaderKHEZOUR <khabdelkaderdjilani@gmail.com>
2023-09-08 18:12:02 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *  * main - program
+ *   *
+ *    * Description: task2
+ *     *
+ *      * Return: 0
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+		{
+		putchar(ch);
+		++ch;
+		}
+		putchar('\n');
+		return (0);
+}
