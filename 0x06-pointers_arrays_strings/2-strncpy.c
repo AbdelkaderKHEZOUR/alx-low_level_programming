ae6cb9b5f5d6355ebaccb111d24363367d7f3058
AbdelkaderKHEZOUR <khabdelkaderdjilani@gmail.com>
2023-09-22 13:45:56 -0700

    thrid task

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  * *_strncpy - Function that copies a string.
+ *   * @dst: string
+ *    * @src: string
+ *     * @n: number of chars to copy over
+ *      * Return: Always 0.
+ */
+char *_strncpy(char *dst, char *src, int n)
+{
+	int a = 0;
+	int b = 0;
+
+	while (a != n)
+	{
+		dst[a] = src[b];
+		if (src[a] == '\0')
+		{
+			dst[b] = '\0';
+			break;
+		}
+		a++;
+		b++;
+	}
+	while (b != n)
+		dest[b++] = '\0';
+	return (dest);
+}
